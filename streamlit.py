034a94b65e3af91e051a948856b873a299b1a178
Flojea <98753660+Flojea@users.noreply.github.com>
2023-02-01 18:53:11 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,25 +1,39 @@
 import streamlit as st
 import requests
-try:
-    from bs4 import BeautifulSoup
-except :
-    from BeautifulSoup import BeautifulSoup 
+from bs4 import BeautifulSoup
 import nltk
 import heapq
 from nltk.tokenize import word_tokenize, sent_tokenize
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+dictionnaire = {"www.lemonde.fr":"article__paragraph", "www.lefigaro.fr":"fig-paragraph",
+               "www.leparisien.fr":"paragraph text_align_left","www.lesechos.fr" : "sc-14kwckt-6 gPHWRV",
+                "www.liberation.fr":"article_link","www.bbc.com":"ssrcss-1q0x1qg-Paragraph eq5iqo00",
+                "www.lequipe.fr" :"Paragraph__content"
+              }
 
 def summarize_article(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
     response = requests.get(url)
     html_content = response.text
     soup = BeautifulSoup(html_content, "html.parser")
-    paragraphs = soup.find_all("p", class_="article__paragraph")
+    if domain in dictionnaire:
+        class_name = dictionnaire[domain]
+        paragraphs = soup.find_all("p", class_=class_name)
+    else:
+    # handle the case where the domain is not in the dictionary
+        paragraphs = []
     article_text = ""
     for paragraph in paragraphs:
         article_text += paragraph.get_text()
+    if ".fr" in url:
+        x="french"
+    else :
+        x = "english"
 
-    stop_words = set(stopwords.words("french"))
+
+    stop_words = set(stopwords.words(x))
     words = word_tokenize(article_text)
     word_frequencies = {}
     for word in words:
