030fc668f98f3e1817a889c1fb0365d2afd292b1
Flojea <98753660+Flojea@users.noreply.github.com>
2023-02-01 15:54:17 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import requests
-from bs4 import BeautifulSoup
+try:
+    from bs4 import BeautifulSoup
+except :
+    from BeautifulSoup import BeautifulSoup 
 import nltk
 import heapq
 from nltk.tokenize import word_tokenize, sent_tokenize
